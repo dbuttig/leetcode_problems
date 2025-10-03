963c7ec7f50f8fb9c42e526f8782764ee37b25b1
Daniel Buttig <daniel.buttig@gmail.com>
2025-10-02 23:20:00 -0500

    Time: 6 ms (37.17%), Space: 211.5 MB (57.85%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.rb
+++ 0014-longest-common-prefix/0014-longest-common-prefix.rb
@@ -1,14 +1,11 @@
 # @param {String[]} strs
 # @return {String}
 def longest_common_prefix(strs)
-    count = 0
     base = strs.shift
     base.chars.each_with_index do |s,i|
-        if strs.all? { |string| string[i] == base[i] }
-            count += 1
-        else
-            return base[0...count]
+        unless strs.all? { |string| string[i] == s }
+            return base[0...i]
         end
     end
-    base[0...count]
+    base
 end
