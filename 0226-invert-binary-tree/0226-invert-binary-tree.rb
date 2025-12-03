ff06d6b37c0581b809481748142d4276698c3328
Daniel Buttig <daniel.buttig@gmail.com>
2025-12-03 09:51:17 -0600

    Time: 0 ms (100%), Space: 211.6 MB (1.09%) - LeetHub

---
+++ 0226-invert-binary-tree/0226-invert-binary-tree.rb
@@ -0,0 +1,22 @@
+# Definition for a binary tree node.
+# class TreeNode
+#     attr_accessor :val, :left, :right
+#     def initialize(val = 0, left = nil, right = nil)
+#         @val = val
+#         @left = left
+#         @right = right
+#     end
+# end
+# @param {TreeNode} root
+# @return {TreeNode}
+def invert_tree(root)
+    return nil if root.nil?
+
+    left_inverted = invert_tree(root.left)
+    right_inverted = invert_tree(root.right)
+
+    root.left = right_inverted
+    root.right = left_inverted
+
+    return root
+end
